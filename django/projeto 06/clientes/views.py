0d1c0576eeca4852174b8644dba5ab4fada9a2e9
Paadafalws <99057639+Paadafalws@users.noreply.github.com>
2023-07-22 21:47:06 -0300

    1

--- django/projeto 06/clientes/views.py
+++ django/projeto 06/clientes/views.py
@@ -1,13 +1,19 @@
-from rest_framework import viewsets,filters
+from rest_framework import viewsets, filters
 from clientes.serializers import ClienteSerializer
 from clientes.models import Cliente
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.filters import SearchFilter
+
 
 class ClientesViewSet(viewsets.ModelViewSet):
     """Listando clientes"""
     queryset = Cliente.objects.all()
     serializer_class = ClienteSerializer
-    filter_backends = [DjangoFilterBackend,filters.OrderingFilter]
-    filterset_fields = ['nome']
+    filter_backends = [filters.OrderingFilter, SearchFilter, DjangoFilterBackend]  # Invertida a ordem dos filtros
+
     search_fields = ['nome', 'cpf']
     ordering_fields = ['nome', 'cpf']  
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
